ca28b33bb2ccc8e91cf723054abb32e3854ff9dd
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2022-10-19 14:09:08 +0200

    improve footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -5,35 +5,31 @@ import styles from './Footer.module.scss'
 
 
 export default function Footer() {
-    return (
-        <footer className={styles.footer}>
-            <div className={`py-5 ${styles.footerTop}`}>
-                <Container className={"text-center"}>
-                    <h4>Développement d{"'"}applications Web à Toulouse et en France</h4>
-                </Container>
-            </div>
-            <div className={`py-4 ${styles.footerBottom}`}>
-                <div className="container">
-                    <div className="column one">
-                        <div className="copyright">
-                            <div>
-                                ©&nbsp;{new Date().getFullYear()} Silarhi. Tous droits réservés
-                                &nbsp;|&nbsp;
-                                <Link href={"/mentions-legales"}>
-                                    <a>Mentions légales</a>
-                                </Link>
-                                &nbsp;|&nbsp;
-                                <Link href={"/conditions-generales-de-vente"}>
-                                    <a>CGV</a>
-                                </Link>
-                            </div>
-                            <div>Site développé par un <a href="https://sainthillier.fr">développeur Web freelance à
-                                Toulouse</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </footer>
-    )
+  return (
+    <footer className={styles.footer}>
+      <div className={`py-5 text-bg-primary-dark`}>
+        <Container className={"text-center"}>
+          <h4>Développement d{"'"}applications Web à Toulouse et en France</h4>
+        </Container>
+      </div>
+      <div className={`py-4 text-bg-primary`}>
+        <div className="container">
+          <div>
+            ©&nbsp;{new Date().getFullYear()} Silarhi. Tous droits réservés
+            &nbsp;|&nbsp;
+            <Link href={"/mentions-legales"}>
+              <a>Mentions légales</a>
+            </Link>
+            &nbsp;|&nbsp;
+            <Link href={"/conditions-generales-de-vente"}>
+              <a>CGV</a>
+            </Link>
+          </div>
+          <div>Site développé par un <a href="https://sainthillier.fr">développeur Web freelance à
+            Toulouse</a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
 }
