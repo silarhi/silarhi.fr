7ac0f73ba1de80046fba4f015addbef65ff9aff8
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2023-04-02 15:20:02 +0200

    minor tweak on footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,13 +6,13 @@ import styles from './Footer.module.scss'
 
 export default function Footer() {
   return (
-    <footer className={styles.footer}>
-      <div className={`py-5 text-bg-primary-dark`}>
+    <footer className={`text-bg-primary ${styles.footer}`}>
+      <div className={`py-5 ${styles.topFooter}`}>
         <Container className={"text-center"}>
           <h4>Développement d{"'"}applications Web à Toulouse et en France</h4>
         </Container>
       </div>
-      <div className={`py-4 text-bg-primary`}>
+      <div className={`py-4 ${styles.subFooter}`}>
         <div className="container">
           <div>
             ©&nbsp;{new Date().getFullYear()} SILARHI. Tous droits réservés
@@ -25,8 +25,9 @@ export default function Footer() {
               CGV
             </Link>
           </div>
-          <div>Site développé par un <a href="https://sainthillier.fr">développeur Web freelance à
-            Toulouse</a>
+          <div>
+            Site développé par un
+            <a href="https://sainthillier.fr">développeur Web freelance à Toulouse</a>
           </div>
         </div>
       </div>
