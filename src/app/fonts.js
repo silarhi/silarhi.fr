aacdd37820e8304192ad30750b68926d234a1424
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2023-06-10 14:35:35 +0200

    use lato for kpi

---
+++ src/app/fonts.js
@@ -0,0 +1,13 @@
+import { Lato, Montserrat } from 'next/font/google'
+
+export const lato = Lato({
+    subsets: ['latin'],
+    weight: ['700'],
+    display: 'swap',
+})
+
+export const montserrat = Montserrat({
+    weight: ['300', '700'],
+    subsets: ['latin'],
+    variable: '--body-font',
+})
