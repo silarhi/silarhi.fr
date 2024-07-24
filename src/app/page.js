00733715c8a684c9fcf8bb238c8c87c6175c20e5
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2024-07-24 18:41:39 +0200

    update employees date

--- src/app/page.js
+++ src/app/page.js
@@ -131,18 +131,21 @@ const EMPLOYEES = [
     {
         name: 'Florian',
         from: new Date('2020-09-01'),
+        to: new Date('2023-09-15'),
         coffeesPerDay: 0,
         hoursPerDay: 7,
     },
     {
         name: 'Rémy',
         from: new Date('2022-01-01'),
+        to: new Date('2024-03-15'),
         coffeesPerDay: 4,
         hoursPerDay: 8,
     },
     {
         name: 'Maxime',
         from: new Date('2022-11-02'),
+        to: new Date('2024-04-01'),
         coffeesPerDay: 3,
         hoursPerDay: 8,
     },
