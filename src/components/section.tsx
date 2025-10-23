c6cb7b8eb602c704d91ae5d4b04cebb08c8f6923
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2025-10-23 22:19:51 +0200

    Fix Section component padding issues

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -5,31 +5,26 @@ import { cn } from '@/utils/lib'
 interface SectionProps extends React.HTMLAttributes<HTMLDivElement> {
     container?: boolean
     fluid?: boolean
-    size?: 'default' | 'xl'
-    paddingX?: boolean
     paddingY?: boolean
 }
 
 export default function Section({
     container = true,
     fluid = false,
-    size: _size = 'default',
     children,
     className,
-    paddingX = true,
     paddingY = true,
     ...props
 }: SectionProps) {
-    const containerClass = fluid ? 'w-full px-4' : 'container mx-auto px-4'
-
-    const sectionClasses = cn(paddingX && 'px-4 lg:px-8 xl:px-16', paddingY && 'py-4 lg:py-8 xl:py-16', className)
+    const sectionClasses = cn(paddingY && 'py-16 lg:py-24', className)
 
     return (
         <section className={sectionClasses} {...props}>
-            <>
-                {container && <div className={cn(containerClass, { 'px-0': paddingX })}>{children}</div>}
-                {!container && children}
-            </>
+            {container ? (
+                <div className={cn('container mx-auto px-4 lg:px-8', { 'w-full': fluid })}>{children}</div>
+            ) : (
+                children
+            )}
         </section>
     )
 }
